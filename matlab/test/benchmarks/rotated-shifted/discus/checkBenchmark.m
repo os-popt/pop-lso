41ef6b760c36be1d92e139ccbb0267523d405a5b
os-popt <11749325@mail.sustech.edu.cn>
2020-01-24 18:30:49 +0800

    fix a small typo

--- matlab/test/benchmarks/rotated-shifted/discus/checkBenchmark.m
+++ matlab/test/benchmarks/rotated-shifted/discus/checkBenchmark.m
@@ -1,5 +1,11 @@
 clear; clc;
 
+% first, download two files which are located in
+%   https://github.com/os-popt/pop-lso/blob/master/matlab/test/benchmarks/shifted/discus/generateShiftVector.m
+%   https://github.com/os-popt/pop-lso/blob/master/matlab/test/benchmarks/rotated/discus/generateRotationMatrix.m
+% into the directory of this script,
+% and then, run them before executing this script.
+
 % $ nohup matlab -nodesktop -nodisplay -nosplash <checkBenchmark.m >checkBenchmark.out 2>&1 &
 
 funcName = 'poRotatedShiftedDiscus';
