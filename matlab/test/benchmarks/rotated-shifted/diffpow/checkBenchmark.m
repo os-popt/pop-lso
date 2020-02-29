91c70c2959e35d89b7bac8eb59edceff3c1d0d49
os-popt <11749325@mail.sustech.edu.cn>
2020-02-29 23:53:27 +0800

    add matlab/test/benchmarks/rotated-shifted/diffpow/checkBenchmark.m

---
+++ matlab/test/benchmarks/rotated-shifted/diffpow/checkBenchmark.m
@@ -0,0 +1,42 @@
+clear; clc;
+
+% first, download two files which are located in
+%   https://github.com/os-popt/pop-lso/blob/master/matlab/test/benchmarks/shifted/diffpow/generateShiftVector.m
+%   https://github.com/os-popt/pop-lso/blob/master/matlab/test/benchmarks/rotated/diffpow/generateRotationMatrix.m
+% into the directory of this script,
+% and then, run them before executing this script.
+
+% $ nohup matlab -nodesktop -nodisplay -nosplash <checkBenchmark.m >checkBenchmark.out 2>&1 &
+
+funcName = 'poRotatedShiftedDiffPow';
+
+%%
+startTime = tic;
+funcDims = [2 5 7 10 20 50 70 100 1e3 1e4];
+for i = 1 : numel(funcDims)
+    funcDim = funcDims(i);
+    X = zeros(funcDim, randi(1e3));
+    y = zeros(1, size(X, 2));
+    poManuallyCheckBenchmark(funcName, X, y, true);
+end
+runtime = toc(startTime);
+fprintf('$ - runtime - $: %9.7e seconds (%7.5E hours).\n', runtime, runtime / 3600);
+
+%%
+startTime = tic;
+poAutomaticallyCheckBenchmark(funcName);
+runtime = toc(startTime);
+fprintf('$ - runtime - $: %9.7e seconds (%7.5E hours).\n', runtime, runtime / 3600);
+
+%%
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the manual checking within the tolerance `1.00e-12`.
+% poRotatedShiftedDiffPow : pass the automatic checking within the tolerance `1.00e-06`.
