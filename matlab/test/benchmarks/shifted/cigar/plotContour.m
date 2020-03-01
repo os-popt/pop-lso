dc63d07a6f8ead04ce5548fffddc3a88247602c5
os-popt <11749325@mail.sustech.edu.cn>
2020-03-01 21:11:48 +0800

    add matlab/test/benchmarks/shifted/cigar/plotContour.m

---
+++ matlab/test/benchmarks/shifted/cigar/plotContour.m
@@ -0,0 +1,9 @@
+clear; clc;
+
+% first, run the script called generateShiftVector.m, which is
+% located in the same directory.
+
+poPlotContour('poShiftedCigar', ...
+    -5 : 0.05 : 5, ...
+    -5 : 0.05 : 5, ...
+    1e6 * (0 : 1 : 9) .^ 2);
