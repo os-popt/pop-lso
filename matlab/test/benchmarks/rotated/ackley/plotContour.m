3b3e7edf284a496af8660fffabb702d2bb1d3a08
os-popt <11749325@mail.sustech.edu.cn>
2020-03-03 22:35:24 +0800

    add matlab/test/benchmarks/rotated/ackley/plotContour.m

---
+++ matlab/test/benchmarks/rotated/ackley/plotContour.m
@@ -0,0 +1,9 @@
+clear; clc;
+
+% first, run the script called generateRotationMatrix.m, which is
+% located in the same directory.
+
+poPlotContour('poRotatedAckley', ...
+    -5 : 0.05 : 5, ...
+    -5 : 0.05 : 5, ...
+    0 : 2 : 14);
