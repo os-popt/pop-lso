5ac6553a874dfb56d2c045dc27f426194fa53895
os-popt <11749325@mail.sustech.edu.cn>
2020-03-06 21:37:27 +0800

    add matlab/test/benchmarks/rotated/discus/plotContour.m

---
+++ matlab/test/benchmarks/rotated/discus/plotContour.m
@@ -0,0 +1,9 @@
+clear; clc;
+
+% first, run the script called generateRotationMatrix.m, which is
+% located in the same directory.
+
+poPlotContour('poRotatedDiscus', ...
+    -5 : 0.05 : 5, ...
+    -5 : 0.05 : 5, ...
+    1e6 * (0 : 0.5 : 5) .^ 2);
