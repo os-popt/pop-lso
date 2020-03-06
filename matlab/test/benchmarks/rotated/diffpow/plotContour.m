e617e6735d79ed97d866448524b90be57d073a1a
os-popt <11749325@mail.sustech.edu.cn>
2020-03-06 21:34:35 +0800

    add matlab/test/benchmarks/rotated/diffpow/plotContour.m

---
+++ matlab/test/benchmarks/rotated/diffpow/plotContour.m
@@ -0,0 +1,9 @@
+clear; clc;
+
+% first, run the script called generateRotationMatrix.m, which is
+% located in the same directory.
+
+poPlotContour('poRotatedDiffPow', ...
+    -5 : 0.05 : 5, ...
+    -5 : 0.05 : 5, ...
+    [0 : 1 : 5 20 : 40 : 100] .^ 2);
